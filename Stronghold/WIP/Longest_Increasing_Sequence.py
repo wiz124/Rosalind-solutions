94349f50a0d71b1ee102b7c8bce431b1f4fb2d97
komiveslab <hli16492@gmail.com>
2025-10-24 17:33:50 -0700

    skipp longestincreasingseq for now

--- Stronghold/WIP/Longest_Increasing_Sequence.py
+++ Stronghold/WIP/Longest_Increasing_Sequence.py
@@ -1,7 +1,39 @@
-#algorithm
+# algorithm
+# 9
+# 8 2 1 6 5 7 4 3 9
+#
+#iterate through each integer in the list(doing nested for loop)
+#               one to iterate for each int in the list
+#               have another loop iterate through the list for every index after array[i]
+# initialize lis=[] and new_lis=[]
+#find longest possible subseq for array[i]
+#
+#
+#
 
-#def Main(n, perm):
-#    for i in perm:
+def longest_seq(n,arr):
+
+    LIS=[]
+
+    #iterate through every number in array
+    for i in range(0,n):
+
+        possible_seq=[arr[i]]*(len(arr)-1-arr.index(i))
+
+        temp_arr=[arr[i]]
+
+        #iterate through every number after index position of number
+        for j in range(arr[i]+1,n):
+
+            #check if next element greater than element  at index i
+            if arr[j]>arr[i]:
+
+                if arr[j]>temp_arr[-1]:
+                    temp_arr.append(arr[j])
+                else:
+                    pass
+
+
 
 
 
@@ -9,28 +41,22 @@
 
 
 #parse input file
-with open('input.txt', 'r') as f:
+with open('input2.txt', 'r') as f:
 
-    n = 0
     perm = []
     strings = []
 
     for line in f:
         strings.append(line.strip())
 
-
 #convert everything to int
-    n = int(strings[0])
-    perm=list(map(int,strings[1].split(' ')))
+n = int(strings[0])
 
-    #Main(n,perm)
-    
+    #map(function, iterable,...) applies function to each iterable
+perm=tuple(map(int,strings[1].split(' ')))
 
-'''
-5
-5 1 4 2 3
-0 1 2 3 4
+longest_seq(n,perm)
 
 
 
-'''
+
--- Stronghold/WIP/test.py
+++ Stronghold/WIP/test.py
@@ -1,6 +1,18 @@
-string=('abcdefg')
+# string=('abcdefg')
+#
+# for i in range(len(string)-1,-1,-1):
+#     print(string[i])
+def map_word_length_to_words(words):
+    length_dict = {}
+    for word in words:
+        length = len(word)
+        if length not in length_dict:
+            length_dict[length] = []
+        else:
+            length_dict[length].append(word)
+    return length_dict
 
-print(string[1:])
 
-for i in range(0,len(string)):
-    print(string[len(string)-i-1])
+words = ["Joy", "Star", "Chill", "Dream", "Hope", "Quick", "Brave", "Fun", "Code", "Smile"]
+length_mapping = map_word_length_to_words(words)
+print(length_mapping)
